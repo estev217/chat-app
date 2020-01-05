74e3b7bdfa462a25dcda4767dd59af6846a67e64
Estevao Fernandes <estevao.fernandes@live.fr>
2020-01-05 21:36:55 +0100

    challenge react quest 2 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact 
+      name = "Jim Ramos"
+      avatar = "https://randomuser.me/api/portraits/men/54.jpg"
+      />
+      <Contact 
+      name = "Marion Stewart"
+      avatar = "https://randomuser.me/api/portraits/women/9.jpg"
+      online
+      />
+      <Contact 
+      name = "Guy Henderson"
+      avatar = "https://randomuser.me/api/portraits/men/86.jpg"
+      />
     </div>
   );
 }
